97b0a199b7621f5fad2d0b0a02b914c8263e12fb
ekat1983 <shumkovakatya@yandex.ru>
2022-08-27 21:15:09 +0300

    echo

--- src/test/java/ru/netology/postman/postmanTest.java
+++ src/test/java/ru/netology/postman/postmanTest.java
@@ -1,5 +1,25 @@
 package ru.netology.postman;
 
-class postmanTest {
+import org.junit.jupiter.api.Test;
 
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class postmanTest {
+    @Test
+    void ReturnTestData() {
+        // Given - When - Then
+// Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("postmanTest data")
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("postmanTest data"))
+        ;
+    }
 }
