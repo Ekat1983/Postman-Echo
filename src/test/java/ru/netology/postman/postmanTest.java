a022b449f8bf64f3c601d41ecc6701d75d8eb3f3
ekat1983 <shumkovakatya@yandex.ru>
2022-08-27 23:42:16 +0300

    echo

--- src/test/java/ru/netology/postman/postmanTest.java
+++ src/test/java/ru/netology/postman/postmanTest.java
@@ -19,7 +19,7 @@ public class postmanTest {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("some value"))
+                .body("data", equalTo("postmanTest data"))
         ;
     }
 }
